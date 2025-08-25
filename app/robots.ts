a9c786847193dbeec83148faa9af91a2b66bc1e0
jenniferbsmith <jenniferbsmith900@gmail.com>
2025-08-25 20:22:23 +0500

    \🔧 Fix Bing robots.txt error: Remove deprecated Host directive that causes validation issues"

--- app/robots.ts
+++ app/robots.ts
@@ -42,7 +42,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: []
       }
     ],
-    sitemap: `${seoConfig.siteUrl}/sitemap.xml`,
-    host: seoConfig.siteUrl
+    sitemap: `${seoConfig.siteUrl}/sitemap.xml`
   };
 }
